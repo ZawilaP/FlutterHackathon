1a4099f7913b997cb6b7b74f70a5e6a9d860b6a3
nelanz <tomaszewicz.na@pg.com>
2023-01-22 19:24:34 +0100

    Added Synapsis logo, yes/no buttons and appbar

--- badambadam/lib/main.dart
+++ badambadam/lib/main.dart
@@ -14,10 +14,11 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
-        title: 'Namer App',
+        title: 'M-CHAT-RF',
         theme: ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+          colorScheme:
+              ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 222, 0, 1)),
         ),
         home: MyHomePage(),
       ),
@@ -35,12 +36,64 @@ class MyHomePage extends StatelessWidget {
     var appState = context.watch<MyAppState>();
 
     return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'graphics/SYNAPSIS_herb_2.png',
+          fit: BoxFit.cover,
+          scale: 2,
+        ),
+      ),
       body: Column(
         children: [
           Text('A random idea:'),
           Text(appState.current.asLowerCase),
+          YesNoButtons()
         ],
       ),
+    );
+  }
+}
+
+class YesNoButtons extends StatelessWidget {
+  const YesNoButtons({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final ButtonStyle style = ElevatedButton.styleFrom(
+      textStyle: const TextStyle(
+        fontSize: 20,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+
+    return Row(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            style: style,
+            onPressed: () {
+              print('Yes');
+            },
+            child: const Text('YES'),
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            style: style,
+            onPressed: () {
+              print('No');
+            },
+            child: const Text('NO'),
+          ),
+        )
+      ],
     );
   }
 }
