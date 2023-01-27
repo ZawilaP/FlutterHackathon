84870cf585ae356370c0381ec130a763fb3c48a0
DzejDzej <zawilaniedzwiecki.p@pg.com>
2023-01-27 14:30:12 +0100

    Changed age check to be on separate page

--- badambadam/lib/routes/ageCheck.dart
+++ badambadam/lib/routes/ageCheck.dart
@@ -15,55 +15,85 @@ class AgeCheckRoute extends StatelessWidget {
       ),
       body: Column(
         children: [
-          YesNoButtons()
+          TextSubmitForm(onSubmit: (value) => print(value))
         ],
       ),
     );
   }
 }
 
-class YesNoButtons extends StatelessWidget {
-  const YesNoButtons({
-    Key? key,
-  }) : super(key: key);
+class TextSubmitForm extends StatefulWidget {
+  const TextSubmitForm({Key? key, required this.onSubmit}) : super(key: key);
+  final ValueChanged<String> onSubmit;
+
+  @override
+  State createState() => _TextSubmitFormState();
+}
+
+class _TextSubmitFormState extends State<TextSubmitForm> {
+  // declare a GlobalKey
+  final _formKey = GlobalKey<FormState>();
+  bool _submitted = false;
+  String _name = '';
+
+  void _submit() {
+    setState(() => _submitted = true);
+    if (_formKey.currentState!.validate()) {
+      widget.onSubmit(_name);
+      Navigator.pushNamed(context, '/survey');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ButtonStyle style = ElevatedButton.styleFrom(
-      textStyle: const TextStyle(
-        fontSize: 20,
-        fontWeight: FontWeight.bold,
+    // Build a Form widget using the _formKey created above.
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          SizedBox(
+            height: 80,
+            child: TextFormField(
+              decoration: const InputDecoration(
+                labelText: 'Enter age of your child in months',
+              ),
+              autovalidateMode: _submitted
+                  ? AutovalidateMode.onUserInteraction
+                  : AutovalidateMode.disabled,
+              // The validator receives the text that the user has entered.
+              validator: (text) {
+                if (text == null || text.isEmpty) {
+                  return 'Can\'t be empty';
+                }
+                if (int.tryParse(text) == null) {
+                  return 'Please enter a numerical value';
+                }
+                if (int.parse(text) < 16 || int.parse(text) > 30) {
+                  return 'Your child needs to be between 16 and 30 months to be eligible for survey';
+                }
+                return null;
+              },
+              onChanged: (text) => setState(() => _name = text),
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            child: ElevatedButton(
+              onPressed: _name.isNotEmpty ? _submit : null,
+              child: Text(
+                'Submit',
+                style: Theme.of(context)
+                    .textTheme
+                    .headline6!
+                    .copyWith(color: Colors.white),
+              ),
+            ),
+          ),
+        ],
       ),
-    );
-
-    return Row(
-      children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            style: style,
-            onPressed: () {
-              // Navigate to the second screen using a named route.
-              Navigator.pushNamed(context, '/survey');
-            },
-            child: const Text('Age within 16-30 months'),
-          ),
-        ),
-        SizedBox(
-          width: 10,
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ElevatedButton(
-            style: style,
-            onPressed: () {
-              // Navigate to the second screen using a named route.
-              Navigator.pop(context);
-            },
-            child: const Text('Age not within 16-30 months'),
-          ),
-        )
-      ],
     );
   }
 }
+
