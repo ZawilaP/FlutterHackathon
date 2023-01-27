3f84c750e95540ce29d816452760728877ee0d85
EU\nowacki.pj <nowacki.pj@pg.com>
2023-01-27 11:15:57 +0100

    first dummy backend, corrections in python to handle unicode somehow

--- badambadam/lib/model.dart
+++ badambadam/lib/model.dart
@@ -1,7 +1,76 @@
-enum NodeDisplayAndDecisionMakingMethod {
-  Simple_Yes_No,
-  OpenTextAnyAnswerWillDo,
-  OneYesWillDoKeepAsking,
-  OneYesWillDoStopAsking,
-  // more to come
+import 'package:flutter/material.dart';
+import 'dart:convert';
+
+import 'package:flutter/services.dart';
+
+class FakeBackendSingleton {
+  static final FakeBackendSingleton _singleton =
+      FakeBackendSingleton._internal();
+
+  factory FakeBackendSingleton() {
+    return _singleton;
+  }
+
+  FakeBackendSingleton._internal();
+
+  Survey? survey;
+
+  Survey getSurvey(String? guid) {
+    // can get a survey from backend (not implemented now) via a guid
+    // or get the survey from the local json file (if guid is null)
+    // if the survey is already loaded, returns it from memory
+    if (guid == null) {
+      if (this.survey == null) {
+        this.survey = Survey();
+        this.survey!.load();
+      }
+      return survey!;
+    } else {
+      throw Exception("GetSurvey(guid) not implemented");
+    }
+  }
+}
+
+class Survey {
+  List<Node> nodes = [];
+  Future<void> load() async {
+    // load survey from json file
+    String data = await rootBundle.loadString("assets/survey_v1.json");
+    final jsonResult = jsonDecode(data);
+    // get all the nodes processed one by one
+    for (var node in jsonResult["questions"]) {
+      Node n = Node(node["id"], node["author"], node["is_top_level"],
+          node["is_inverted"], node["node_type"], node["questions"]);
+      nodes.add(n);
+      print(n);
+    }
+  }
+}
+
+class Node {
+  late String id;
+  late String author;
+  late bool isTopLevel;
+  late bool isInverted;
+  late String nodeType;
+  String? noPath;
+  String? yesPath;
+  String? thirdPath;
+  List<String> questions = [];
+  Node(String _id, String _author, String _isTopLevel, String _isInverted,
+      String _nodeType, List<dynamic> _questions) {
+    id = _id;
+    author = _author;
+    isTopLevel = _isTopLevel == "YES" ? true : false;
+    isInverted = _isInverted == "YES" ? true : false;
+    nodeType = _nodeType;
+    questions = [];
+    for (var q in _questions) {
+      questions.add(q.toString());
+    }
+  }
+  @override
+  String toString() {
+    return "Node: $id, $author, $isTopLevel, $isInverted, $nodeType, $questions";
+  }
 }
