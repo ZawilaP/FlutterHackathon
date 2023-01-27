307be27de64e45fc0850d40028b98532461fb6d6
DzejDzej <zawilaniedzwiecki.p@pg.com>
2023-01-27 12:51:39 +0100

    Added advanced results route

---
+++ badambadam/lib/routes/advancedResultRoute.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class AdvancedResultRoute extends StatelessWidget {
+  const AdvancedResultRoute({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          'graphics/SYNAPSIS_herb_2.png',
+          fit: BoxFit.cover,
+          scale: 2,
+        ),
+      ),
+      body: Column(
+        children: [
+          Text("Where now?")
+        ],
+      ),
+    );
+  }
+}
