7cf0d397594ddd26cfbbb99f5c7ff77e55389cd6
nelanz <tomaszewicz.na@pg.com>
2023-02-02 12:20:43 +0100

    Fromatted results

--- badambadam/lib/main.dart
+++ badambadam/lib/main.dart
@@ -29,12 +29,17 @@ class MyApp extends StatelessWidget {
         theme: ThemeData(
           useMaterial3: true,
           fontFamily: 'Poppins',
-          colorScheme:
-              ColorScheme.fromSeed(seedColor: Color.fromRGBO(255, 178, 0, 1), primary: Color.fromRGBO(255, 178, 0, 1), onPrimary: Color.fromRGBO(45, 42, 40, 1)),
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: Color.fromRGBO(255, 178, 0, 1),
+              primary: Color.fromRGBO(255, 178, 0, 1),
+              onPrimary: Color.fromRGBO(45, 42, 40, 1)),
           textTheme: const TextTheme(
             displayLarge:
                 TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-            titleLarge: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold, fontFamily: 'RobotoCondensed'),
+            titleLarge: TextStyle(
+                fontSize: 20.0,
+                fontWeight: FontWeight.bold,
+                fontFamily: 'RobotoCondensed'),
             bodyMedium: TextStyle(fontSize: 18.0),
           ),
         ),
@@ -76,21 +81,18 @@ class MyHomePage extends StatelessWidget {
                 onTap: () {
                   Navigator.pushNamed(context, '/login');
                 },
-                child: Icon(
-                    Icons.login
-                ),
-              )
-          ),
+                child: Icon(Icons.login),
+              )),
         ],
       ),
       body: ListView(
         children: [
-      Text.rich(
-      TextSpan(
-      style: TextStyle(fontSize: 14,),
-        children: [
-          TextSpan(
-              text: """Szanowni Państwo,
+          Text.rich(TextSpan(
+              style: TextStyle(
+                fontSize: 14,
+              ),
+              children: [
+                TextSpan(text: """Szanowni Państwo,
 
 wiemy, że to właśnie Rodzice mają najlepsze intuicje, co do rozwoju swoich dzieci. Większość ich spostrzeżeń dotyczy umiejętności oraz zainteresowań dzieci i jest powodem do radości. Czasem, jako rodzice, mamy jednak także jakieś intuicje i obserwacje budzące nasz niepokój. Zawsze warto je rozwiać, żeby nic nie burzyło przygody, jaką jest wspólne odkrywania świata z naszym Maluchem.
 
@@ -98,50 +100,56 @@ Oddajemy w Państwa ręce narzędzie M-CHAT-R, które zostało stworzone w celu 
 
 Zachęcamy więc do wypełnienia kwestionariusza, szczególnie, że można to zrobić w najbardziej wygodnym dla siebie czasie i miejscu, ze względu na dostępność badania online.
 
-The Modified Checklist for Autism in Toddlers (M-CHAT-R), powiązane z nim materiały oraz wszelkie informacje o możliwości ich użytkowania dostępne są na stronie """
-          ),
-          TextSpan(
-              style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
-              //make link blue and underline
-              text: "www.mchatscreen.com",
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async {
-                  //on tap code here, you can navigate to other page or URL
-                  String url = "https://www.mchatscreen.com";
-                  var urllaunchable = await canLaunch(url); //canLaunch is from url_launcher package
-                  if(urllaunchable){
-                    await launch(url); //launch is from url_launcher package to launch URL
-                  }else{
-                    print("URL can't be launched.");
-                  }
-                }
-          ),
-          TextSpan(
-              text: """
+The Modified Checklist for Autism in Toddlers (M-CHAT-R), powiązane z nim materiały oraz wszelkie informacje o możliwości ich użytkowania dostępne są na stronie """),
+                TextSpan(
+                    style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline),
+                    //make link blue and underline
+                    text: "www.mchatscreen.com",
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        //on tap code here, you can navigate to other page or URL
+                        String url = "https://www.mchatscreen.com";
+                        var urllaunchable = await canLaunch(
+                            url); //canLaunch is from url_launcher package
+                        if (urllaunchable) {
+                          await launch(
+                              url); //launch is from url_launcher package to launch URL
+                        } else {
+                          print("URL can't be launched.");
+                        }
+                      }),
+                TextSpan(
+                  text: """
 
 
 Narzędzie M-CHAT-R przeznaczone jest dla dzieci w wieku 16 do 30 miesięcy. Wypełnienie kwestionariusza dla dzieci młodszych niż 16 miesięcy da nieprawdziwe wyniki, gdyż mają one prawo nie opanować jeszcze wielu umiejętności, jakie posiadają dzieci starsze. Rodziców młodszych dzieci (pomiędzy 12 a 18 miesiącem życia) zachęcamy do zapoznania się z listą umiejętności rozwojowych odpowiadających temu wiekowi """,
-          ),
-          TextSpan(
-              style: TextStyle(color: Colors.blue, decoration: TextDecoration.underline),
-              //make link blue and underline
-              text: "tutaj.",
-              recognizer: TapGestureRecognizer()
-                ..onTap = () async {
-                  //on tap code here, you can navigate to other page or URL
-                  String url = "http://synapsis.org.pl/zycie-z-autyzmem/dla-rodzicow-i-opiekunow/12-18-miesiac";
-                  var urllaunchable = await canLaunch(url); //canLaunch is from url_launcher package
-                  if(urllaunchable){
-                    await launch(url); //launch is from url_launcher package to launch URL
-                  }else{
-                    print("URL can't be launched.");
-                  }
-                }
-          ),
-          TextSpan(text: "\n\nBy wziąć udział w ankiecie wypełnij wiek swojego dziecka w miesiącach poniżej:"),
-        ]
-    )
-    ),
+                ),
+                TextSpan(
+                    style: TextStyle(
+                        color: Colors.blue,
+                        decoration: TextDecoration.underline),
+                    //make link blue and underline
+                    text: "tutaj.",
+                    recognizer: TapGestureRecognizer()
+                      ..onTap = () async {
+                        //on tap code here, you can navigate to other page or URL
+                        String url =
+                            "http://synapsis.org.pl/zycie-z-autyzmem/dla-rodzicow-i-opiekunow/12-18-miesiac";
+                        var urllaunchable = await canLaunch(
+                            url); //canLaunch is from url_launcher package
+                        if (urllaunchable) {
+                          await launch(
+                              url); //launch is from url_launcher package to launch URL
+                        } else {
+                          print("URL can't be launched.");
+                        }
+                      }),
+                TextSpan(
+                    text:
+                        "\n\nBy wziąć udział w ankiecie wypełnij wiek swojego dziecka w miesiącach poniżej:"),
+              ])),
           TextSubmitForm(onSubmit: (value) => print(value)),
           ElevatedButton(
             onPressed: () {
