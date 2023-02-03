a290ebb1bb8dc13f476e7b999347c9667b666680
DzejDzej <zawilaniedzwiecki.p@pg.com>
2023-02-03 12:41:09 +0100

    Fixed formatting of all surveys

--- badambadam/lib/routes/adminPanelRoute.dart
+++ badambadam/lib/routes/adminPanelRoute.dart
@@ -46,6 +46,49 @@ class _AdminPanelRoute extends State<AdminPanelRoute> {
       );
     }
 
+    createFutureBuild() {
+      return FutureBuilder(
+          future: getSurveyAnswers(),
+          initialData: "Loading text..",
+          builder: (BuildContext context, AsyncSnapshot<dynamic> text) {
+            return new SingleChildScrollView(
+                padding: new EdgeInsets.all(8.0),
+                child: new Text(
+                  text.data.toString().replaceAll("],", "],\n").replaceAll("{", "").replaceAll("}", ""),
+                  style: new TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 19.0,
+                  ),
+                ));
+          });
+    }
+
+    Future<void> _showMyDialog2() async {
+      return showDialog<void>(
+        context: context,
+        barrierDismissible: false, // user must tap button!
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text('All surveys:'),
+            content: SingleChildScrollView(
+                child: createFutureBuild()
+            ),
+            actions: <Widget>[
+              TextButton(
+                child: const Text(
+                  'Close',
+                  style: TextStyle(fontSize: 20),
+                ),
+                onPressed: () {
+                  Navigator.of(context).pop();
+                },
+              ),
+            ],
+          );
+        },
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: Image.asset(
@@ -64,11 +107,11 @@ class _AdminPanelRoute extends State<AdminPanelRoute> {
             child: const Text('Edit Questions'),
           ),
           ElevatedButton(
-            onPressed: () {
-              // Navigate to the second screen using a named route.
-              Navigator.pushNamed(context, '/surveys');
+            child: Text("Show all past surveys"),
+            onPressed: () async {
+              print(await getAnswers());
+              _showMyDialog2();
             },
-            child: const Text('See all surveys'),
           ),
           Divider(),
           Form(
@@ -144,3 +187,6 @@ class _AdminPanelRoute extends State<AdminPanelRoute> {
   }
 }
 
+dynamic getAnswers() async {
+  return await getSurveyAnswers().then((id) => [id]);
+}
