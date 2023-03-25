314c9477a85eddfcb5f0e942aae8c4a3bca5df39
nelanz <tomaszewicz.na@pg.com>
2023-03-25 18:33:36 +0100

    Added some texts to advanced result screen'

---
+++ badambadam/lib/screens/resultScreen/advanced_result_texts_pl.dart
@@ -0,0 +1,21 @@
+const negativeIntro = 'Twoje dziecko otrzymało wynik ujemny.';
+const positiveIntro = 'Twoje dziecko otrzymało wynik dodatni.';
+
+const negativeParagraph =
+    'Jeśli masz podejrzenia, że Twoje dziecko może mieć ASD, powinno ono zostać poddane dalszej ocenie, niezależnie od wyniku badania.';
+const positiveParagraph =
+    'Zaleca się niezwłoczne skierowanie go na wczesną interwencję i testy diagnostyczne.';
+
+const negativeActions = [
+  'Pamiętaj:',
+  'Jeśli Twoje dziecko w chwili badania nie ukończyło jeszcze 24 miesięcy, zalecane jest powtórne wypełnienie kwestionariusza M-CHAT-R pomiędzy 24 a 30 miesiącem życia.',
+  'Możesz skorzystać z bezpłatnej teleporady w ramach kontraktu z NFZ. W celu umówienia się wypełnij ankietę: https://ankieta.synapsis.waw.pl/ankieta/',
+  'Więcej informacji na temat umiejętności, które dziecko w tym wieku powinno posiadać oraz wspierania jego rozwoju znajdziesz na stronie http://badabada.pl/dla-rodzicow/rozwoj-dziecka'
+];
+const positiveActions = [
+  'Co możesz teraz zrobić:',
+  'Skontaktuj się ze specjalistą zajmującym się diagnozowaniem spektrum autyzmu u małych dzieci, aby porozmawiać i omówić wyniki badania oraz wyjaśnić swoje wątpliwości',
+  'Skorzystaj z bezpłatnej teleporady w ramach kontraktu z NFZ. W celu umówienia się wypełnij ankietę: https://ankieta.synapsis.waw.pl/ankieta/',
+  'W oczekiwaniu na konsultację ze specjalistą lub diagnozę: prowadź notatki o zdobywanych przez dziecko umiejętnościach i zachowaniach, które Cię niepokoją, nagrywaj swoje dziecko podczas zabawy, w kontakcie z osobami bliskimi i innymi dziećmi, zgłoś się do swojej rejonowej Poradni Psychologiczno-Pedagogicznej',
+  'Więcej informacji na temat umiejętności, które dziecko w tym wieku powinno posiadać oraz wspierania jego rozwoju znajdziesz na stronie http://badabada.pl/dla-rodzicow/rozwoj-dziecka'
+];
